a7ce68e3b896d22c456d3f42b29b8cbf524077a6
kvchi <jonathanmkpuma@gmail.com>
2024-08-27 20:00:53 +0100

    support change

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -7,7 +7,7 @@ export default function Support() {
     <div className="w-8 h-8 p-2 bg-white rounded-full transition-colors duration-300 group-hover:bg-[#6AADF0]/50 border  box-content  ">
       <img src={bot} alt="Support Icon" className="w-full h-full" />
     </div>
-    <p className="text-sm md:text-xl font-medium text-white group-hover:-translate-y-0.5 transform transition group-hover:text-black pr-3">Support</p>
+    <p className="text-sm md:text-xl font-medium text-white group-hover:animate-shake transform transition group-hover:text-black pr-3">Support</p>
   </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -8,10 +9,20 @@ export default {
     extend: {
       colors: {
         primary: "#2E95F4",
-        secondary:"#F0F6FB",
+        secondary: "#F0F6FB",
         red: "#FFE7E7",
         yell: "#E7D37F"
-    },
+      },
+      keyframes: {
+        shake: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '25%': { transform: 'translateX(-1px)' },
+          '75%': { transform: 'translateX(1px)' },
+        },
+      },
+      animation: {
+        shake: 'shake 0.3s ease-in-out',
+      },
     },
   },
   plugins: [
